20ac0a83e79c7df1e805186cd69f2354fbba0ed5
Harmony <harmonymukolwe@gmail.com>
2024-08-29 16:07:32 +0300

    Creates list comprehensions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    if len(num_list) == 0:
+        return []
+    even_num_list = [num for num in num_list if num % 2 == 0]
+    return even_num_list
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_sentence_list = [word + '!' for word in sentence_list]
+    return exclamation_sentence_list
+
+print(return_evens([0,1,2,3,4,5,6,7,8,9]))
